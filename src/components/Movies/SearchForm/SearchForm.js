fa52193a9ea753b813042e00ab4f5d0e4a69b701
Olecya <OlecyaMarkina@yandex.ru>
2023-06-20 18:35:44 +0300

    corr package.json

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -2,8 +2,19 @@ import React, { useState } from "react";
 import './searchForm.css';
 
 export const SearchForm = () => {
-
+    const [word, setWord] = useState('');
     const [filterCheckbox, setFilterCheckbox] = useState(false);
+// /(^[a-z]{2,}$\b)|([\u0401\u0451\u0410-\u044f]$\b)/i;
+    const handleChange = (e) => {
+        // const wordPattern = /(^[a-z]{2,}$)|(^[ёа-я]{2,}$)/;
+        // console.log(wordPattern.test(e.target.value))
+        // wordPattern.test(e.target.value) ? setWord(e.target.value) : setWord('');        
+    }
+
+    const handleSubmit =(e) => {
+        // e.preventDefault();
+        // console.log(word);
+    }
 
     const filterCheckboxButton = () => {
         //TODO filter
@@ -14,8 +25,8 @@ export const SearchForm = () => {
         < >
             <section className="searchForm">
                 <form className="searchForm__form">
-                    <input type="text" className="searchForm__input" placeholder="Фильм" required />
-                    <button className="searchForm__but-search" type="submit">Найти</button>
+                    <input type="text" className="searchForm__input" placeholder="Фильм" required onChange={(e) => { handleChange(e) }} />
+                    <button className={`searchForm__but-search ${word && "searchForm__but-search_activ"}`} type="submit" onClick={(e)=>handleSubmit(e)}>Найти</button>
                 </form>
                 <div className="searchForm__filterCheckbox">
                     <button
